509c9c96269c19b600fc97d2e66f3a55423aaa75
Cologler <skyoflw@gmail.com>
2023-04-15 13:20:05 +0800

    添加项目文件。

---
+++ Vogen_issue_394/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+using Vogen_issue_394;
+
+try
+{
+    using var ctx = new SomeDbContext();
+    ctx.SomeEntities.Add(new());
+    ctx.SaveChanges();
+    Debug.Assert(false);
+}
+catch (Vogen.ValueObjectValidationException)
+{
+    // should not throw for unset id
+    Debug.Assert(true);
+}
+
+try
+{
+    using var ctx = new SomeDbContext();
+    ctx.SomeEntities.Add(new SomeEntity
+    {
+        SomeId = SomeId.Unset
+    });
+    ctx.SomeEntities.Add(new SomeEntity
+    {
+        SomeId = SomeId.Unset
+    });
+    ctx.SaveChanges();
+    Debug.Assert(false);
+}
+catch (InvalidOperationException)
+{
+    // should allow add entity
+    Debug.Assert(true);
+}
+
+Debug.Assert(true);
+
---
+++ Vogen_issue_394/SomeDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+
+using Vogen;
+
+namespace Vogen_issue_394;
+
+internal class SomeDbContext : DbContext
+{
+    public DbSet<SomeEntity> SomeEntities { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("SomeDB");
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder
+            .Properties<SomeId>()
+            .HaveConversion<SomeId.EfCoreValueConverter>();
+    }
+}
+
+[ValueObject(conversions: Conversions.EfCoreValueConverter)]
+[Instance("Unset", 0)]
+public partial struct SomeId { }
+
+public class SomeEntity
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public SomeId SomeId { get; set; }
+}
